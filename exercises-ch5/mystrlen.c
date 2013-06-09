0919ba9c8318b212d7a9a7af0619ffe16c3b9dbb
Geert Gerritsen <ggerritsen@ebay.com>
2013-06-09 20:51:59 +0200

    Ch5: Added mystrlen

---
+++ exercises-ch5/mystrlen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int mystrlen(char input[]) {
+  if (input[0] == 0) return 0;
+  else return 1 + mystrlen(&input[1]);
+}
+
+int main(int argc, char *argv[]) {
+  char userInput[30];
+  strcpy(userInput, argv[1]);
+
+  printf("Length of the string '%s' is: %d\n", userInput, mystrlen(userInput));
+  return 0;
+}
