d1acbd8bbf0d57f265f5b903f23ea1e2d747a768
Geert Gerritsen <ggerritsen@ebay.com>
2013-07-08 22:28:43 +0200

    Ch6: Made database with dynamic size

--- exercises-ch6/simpledb.c
+++ exercises-ch6/simpledb.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_NUMBER_OF_PEOPLE (100)
-#define ERROR_DATABASE_FULL (-1)
+#define CHUNK_SIZE (5)
+#define ERROR_OUT_OF_MEMORY (-2)
 
 typedef struct {
   char *name;
@@ -20,13 +20,29 @@ void finalize_person(person_t *p) {
   free(p->address);
 }
 
-person_t database[MAX_NUMBER_OF_PEOPLE];
+person_t *database = NULL;
 int current_index = 0;
+size_t current_size = 0;
 
-int insert_person(person_t *p) {
-  if (current_index == MAX_NUMBER_OF_PEOPLE) {
-    return ERROR_DATABASE_FULL;
+int insert_person(const person_t *p) {
+  
+  // first call - initialize database
+  if (!database) {
+    database = malloc(CHUNK_SIZE * sizeof(person_t));
+    if (!database) 
+      return ERROR_OUT_OF_MEMORY;
+    current_size = CHUNK_SIZE;
   }
+
+  // database is full
+  if (current_index == current_size) {
+    size_t new_size = current_size * CHUNK_SIZE;
+    person_t *new_space = realloc(database, new_size * sizeof(person_t));
+    if (!new_space) 
+      return ERROR_OUT_OF_MEMORY;
+    database = new_space;
+  }
+
   database[current_index++] = *p;
   return 0;
 }
@@ -35,11 +51,11 @@ int main(void) {
 
   person_t *insertPerson = malloc(sizeof(person_t));
   char input[256];
-  char defaultAddress[] = "address";
+  char address[256];
   
-  printf("Insert a new person\n");
-  scanf("%s", input);
-  init_person(insertPerson, &input[0], &defaultAddress[0]);
+  printf("Insert a new person with address\n");
+  scanf("%s %s", input, address);
+  init_person(insertPerson, &input[0], &address[0]);
   printf("Inserted person: %s, %s\n", insertPerson->name, insertPerson->address);
   
   return 0;
